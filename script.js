cc1b0b92869943c6b71eced3abf93e93036f5b02
Spenser Holstein <spenser.holstein@gmail.com>
2018-01-21 21:15:00 -0500

    next indicator, face and text animation working. no user input

--- script.js
+++ script.js
@@ -5,23 +5,29 @@
 
 let textDisplay = document.getElementById('textDisplay')
 let technoPortrait = document.getElementById('technoFace')
+let userCursor = document.getElementById('userCursor')
+let nextIndicator = document.getElementById('nextIndicator')
+
+
 
 
 ///////////////////////// SET GLOBAL VARIABLES //////////////////////////
-let sampleText = "Hello World"
-let toPortraitAnimate = 1
 
+let textCount = 0
+let sampleText = [
+  "Greetings human.",
+  "I am Technotron-4000, human-computer relations.",
+  "It is my purpose to serve the user of this terminal.",
+  "Please enter in your username."
+]
 
+let toPortraitAnimate = -1
+let animatePortraitOn = null
 
+let nextBlink = 1
+let animateNextOn = null
 
-
-
-
-
-////////////////////////// PORTRAIT STATE ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-
+let cursorBlink = 1
 
 
 
@@ -38,6 +44,11 @@ function animateText(str) {
   let textArray = []
   let finalText = ""
 
+  // hide nextIndicator
+  // nextIndicator.style.visibility = 'hidden'
+  clearInterval(animateNextOn)
+  nextIndicator.style.visibility = 'hidden'
+
   // define function add timeout letters
   function addLetters() {
 
@@ -49,7 +60,7 @@ function animateText(str) {
         document.getElementById('boop').play()
         count += 1
         addLetters()
-      }, 160)
+      }, 60)
     } else if (textSplit[count] === " " && count < textSplit.length ) {
         textArray.push(textSplit[count])
         finalText = textArray.join("")
@@ -58,6 +69,7 @@ function animateText(str) {
         addLetters()
     } else if ( count >= textSplit.length ) {
         animatePortraitOff()
+        animateNextOn = setInterval( setNext, 600)
     }
 
   }
@@ -69,24 +81,23 @@ function animateText(str) {
 
 
 
-////////////////////////// ANIMATE PORTRAIT ON //////////////////////////
+
+
+////////////////////////// PORTRAIT STATE ///////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
-function animatePortraitOn () {
 
-  // let faceAni = setInterval( ()=> {
-  //   toPortraitAnimate *= -1
-  //   console.log(toPortraitAnimate)
-  //
-  //   if (toPortraitAnimate == -1) {
-  //     technoPortrait.style.backgroundImage = "url('images/TT-closed-4.png')"
-  //   } else if (toPortraitAnimate == 1) {
-  //     technoPortrait.style.backgroundImage = "url('images/TT-open-4.png')"
-  //   }
-  //
-  // }, 140)
+function animateFace () {
+      toPortraitAnimate *= -1
 
+      if (toPortraitAnimate == -1) {
+        technoPortrait.style.backgroundImage = "url('images/TT-closed-4.png')"
+      } else if (toPortraitAnimate == 1) {
+        technoPortrait.style.backgroundImage = "url('images/TT-open-4.png')"
+      }
 }
+
+
 
 
 ////////////////////////// ANIMATE PORTRAIT OFF //////////////////////////
@@ -94,19 +105,96 @@ function animatePortraitOn () {
 
 function animatePortraitOff () {
   clearInterval(animatePortraitOn)
+  technoPortrait.style.backgroundImage = "url('images/TT-closed-4.png')"
 }
 
 
 
 
-////////////////////////// ANIMATE ALL //////////////////////////////////
+////////////////////////// CURSOR STATE /////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
-function animateAll (str) {
+
+function setCursor () {
+      cursorBlink *= -1
+
+      if (cursorBlink === -1) {
+        userCursor.style.backgroundColor = "black"
+      } else if (cursorBlink === 1) {
+        userCursor.style.backgroundColor = "rgb(108, 249, 56)"
+      }
+}
+
+
+
+
+////////////////////////// NEXT STATE ///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+function setNext () {
+      nextBlink *= -1
+
+      if (nextBlink === 1) {
+        nextIndicator.style.visibility = 'hidden';
+      } else if (nextBlink === -1) {
+        nextIndicator.style.visibility = 'visible';
+      }
+}
+
+
+////////////////////////// ANIMATE NEXT OFF //////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+function animateNextOff () {
+  clearInterval(animateNextOn)
+  nextIndicator.style.visibility = 'hidden'
+}
+
+
+
+
+////////////////////////// ANIMATE ALL ON LOAD //////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+window.onload = function() {
+
+  nextIndicator.style.visibility = 'hidden';
+
+  let animateCursor = setInterval( setCursor, 400)
+
+  setTimeout( ()=> {
+
+    let str = sampleText[textCount]
+
+    animateText(str)
+    animatePortraitOn = setInterval( animateFace, 120)
+
+    textCount += 1
+
+  }, 600 )
+
+
+
+}
+
+
+
+////////////////////////// ANIMATE ALL ON CLICK /////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+function animateAll () {
+
+  let str = sampleText[textCount]
 
   animateText(str)
-  animatePortraitOn()
+  animatePortraitOn = setInterval( animateFace, 120)
+
+  textCount += 1
 
 }
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
+
+
+
+//
